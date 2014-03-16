3e762c34d7a4ef40aadc66b97212079bff8c4d0d
Johannes Schindelin <johannes.schindelin@gmx.de>
2014-03-15 19:54:07 -0500

    Add a workaround for JUNITBENCH-54
    Reflection is often not the correct way to do things, but sometimes it
    cannot be helped, for example when JUnit does not give us a chance to
    measure the time the original test statement without including the
    runtime of the methods annotated with @Before and @After.
    
    The problem is that JUnit wraps statements into, say, a RunAfter
    statement that ensures that not only the test statement in question is
    executed but also the @After-annotated methods. The same is done for
    @Before methods, and for a couple of other things, so that eventually
    the test statement about which we want to gather statistics is wrapped
    into multiple layers of wrapping statements like an onion into its seven
    skins.
    
    With this commit, we cheat and access the private fields of RunAfter and
    friends, and inject our own BenchmarkStatement (wrapping the statement
    we desire to wrap) into the inner-most 'onion skin'.
    
    We also handle the other statements JUnit 4.11 might wrap around our
    desired test statement:
    
    https://github.com/junit-team/junit/blob/r4.11/src/main/java/org/junit/runners/BlockJUnit4ClassRunner.java#L251
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src/main/java/com/carrotsearch/junitbenchmarks/BenchmarkRule.java
+++ src/main/java/com/carrotsearch/junitbenchmarks/BenchmarkRule.java
@@ -1,6 +1,12 @@
 package com.carrotsearch.junitbenchmarks;
 
+import java.lang.reflect.Field;
+
 import org.junit.Rule;
+import org.junit.internal.runners.statements.ExpectException;
+import org.junit.internal.runners.statements.FailOnTimeout;
+import org.junit.internal.runners.statements.RunAfters;
+import org.junit.internal.runners.statements.RunBefores;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
@@ -43,6 +49,68 @@ public final class BenchmarkRule implements TestRule
      */
     @Override
     public Statement apply(Statement base, Description description) {
+        try {
+            if (workAroundWrappedStatements(base, description)) {
+                return base;
+            }
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         return new BenchmarkStatement(base, description, consumers);
+    }
+
+    /**
+     * Works around a JUnit limitation regarding &#x40;Before and &#x40;After.
+     * <p>
+     * When JUnit hands a {@link Statement} to the {@link BenchmarkRule}, it might
+     * not be the original statement, but rather a statement that is wrapped in
+     * statements ensuring that the methods annotated with &#x40;Before and/or
+     * &#x40;After are run. This method works around that by unwrapping the
+     * original statement, via reflection.
+     * </p>
+     *
+     * @param statement the statement to benchmark, possibly wrapped
+     * @param description the description
+     * @return whether a workaround was necessary
+     * @throws NoSuchFieldException
+     * @throws SecurityException
+     * @throws IllegalAccessException
+     * @throws IllegalArgumentException
+     */
+    private boolean workAroundWrappedStatements(Statement statement,
+            Description description) throws SecurityException, NoSuchFieldException,
+                IllegalArgumentException, IllegalAccessException
+    {
+        Field field = null;
+        Object object = null;
+        if (statement instanceof RunAfters) {
+            object = statement;
+            field = object.getClass().getDeclaredField("fNext");
+            field.setAccessible(true);
+            statement = (Statement) field.get(object);
+        }
+        if (statement instanceof RunBefores) {
+            object = statement;
+            field = object.getClass().getDeclaredField("fNext");
+            field.setAccessible(true);
+            statement = (Statement) field.get(object);
+        }
+        if (statement instanceof FailOnTimeout) {
+            object = statement;
+            field = object.getClass().getDeclaredField("fOriginalStatement");
+            field.setAccessible(true);
+            statement = (Statement) field.get(object);
+        }
+        if (statement instanceof ExpectException) {
+            object = statement;
+            field = object.getClass().getDeclaredField("fNext");
+            field.setAccessible(true);
+            statement = (Statement) field.get(object);
+        }
+
+        if (field == null) return false;
+        field.set(object, new BenchmarkStatement(statement, description, consumers));
+        return true;
     }
 }
